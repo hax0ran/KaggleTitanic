14595a997ded0194862cbced88dea272836e0be8
hax0ran <hax0ran@users.noreply.github.com>
2017-04-27 08:10:53 -0400

    Add files via upload

---
+++ resultsconv.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class resultsconv{
+	public static void main(String[] args){
+		readFile("C:\\Users\\M\\Desktop\\resultss.csv");
+	}
+	
+	public static List<String> readFile(String filename)
+	{
+	  List<String> records = new ArrayList<String>();
+	  try
+	  {
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		String line;
+		BufferedWriter writer= new BufferedWriter(new FileWriter("results.csv"));
+		writer.write("PassengerId,Survived\n");
+		while ((line = reader.readLine()) != null)
+		{
+		  writer.write((Integer.parseInt(line.substring(0,line.indexOf(",")))+891)+line.substring(line.indexOf(","),line.indexOf(",")+1)+line.substring(line.indexOf(":")+1,line.length())+"\n");
+		}
+		reader.close();
+		writer.close();
+		return records;
+	  }
+	  catch (Exception e)
+	  {
+		System.err.format("Exception occurred trying to read '%s'.", filename);
+		e.printStackTrace();
+		return null;
+	  }
+	}
+}
